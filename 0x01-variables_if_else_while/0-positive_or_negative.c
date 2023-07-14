7ee6f7935edee5699bae47f7275633f954d29621
Moustapha iourjdil <moustapha.iourjdil@gmail.com>
2023-07-14 09:53:04 -0700

    0. Positive anything is better than negative nothing

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -5,7 +5,7 @@
 /**
  * main - this program prints a random number
  * Description: The output of the program should be
- * The number, followed by
+ * Return: The number, followed by
  * if the number is greater than 0: is positive
  * if the number is 0: is zero
  * if the number is less than 0: is negative
