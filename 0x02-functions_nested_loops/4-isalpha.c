15538e2607ebb757ac62c3382480e68eb3295e43
Moustapha iourjdil <moustapha.iourjdil@gmail.com>
2023-07-18 17:29:10 -0700

    4. isalpha

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * _isalpha - This program checks if input
+ * @c: is alpha
+ * Return: 1 if True, 0 if False
+ */
+int _isalpha(int c)
+{
+	if (c >= 97 && c <= 122)
+	{
+		return (1);
+	}
+	else if (c >= 65 && c <= 90)
+	{
+		return (1);
+	}
+	return (0);
+}
