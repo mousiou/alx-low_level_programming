8051a1e64ad3abaaea4e25f6667f50a9f55d91d4
moustapha iourjdil <mousapha.iourjdil@gmail.com>
2023-12-11 15:42:35 -0800

    dynamic_libraries

---
+++ 0x18-dynamic_libraries/100-operations.c
@@ -0,0 +1,24 @@
+int add(int i, int k)
+{
+	return (i + k);
+}
+
+int sub(int i, int k)
+{
+	return (i - k);
+}
+
+int mul(int i, int k)
+{
+	return (i * k);
+}
+
+int div(int i, int k)
+{
+	return (i / k);
+}
+
+int mod(int i, int k)
+{
+	return (i % k);
+}
